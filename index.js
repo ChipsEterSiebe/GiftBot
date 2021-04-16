36d0d62a6fdf714e378914bf5d705b57c4c347e1
ChipsEterSiebe <78964881+ChipsEterSiebe@users.noreply.github.com>
2021-04-16 20:28:05 +0200

    init
    init

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const generator = require('generate-password');
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
+port = process.env.PORT || 80
 
 client.on('ready', () => {
  client.user.setActivity("Generating Nitro!");
@@ -28,6 +24,6 @@ client.on("message", async message => {
     i++;
 	}
   }
-}).listen(process.env.PORT || 5000)
+})
 
 client.login(process.env.token);
