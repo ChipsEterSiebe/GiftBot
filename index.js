211dab9b41b3cf72cc0ea663095a71dd41372163
ChipsEterSiebe <78964881+ChipsEterSiebe@users.noreply.github.com>
2021-05-04 19:42:38 +0200

    init
    init

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const generator = require('generate-password');
+const fs = require("fs");
 
+client.on("ready", () => {
+  client.user.setActivity(`Currently in ${client.guilds.cache.size} server(s)`);
+  console.log("Bot (" + client.user.tag + ") Ready!");
+});
 
-client.on('ready', () => {
-client.user.setActivity(`Currently in ${client.guilds.cache.size} server(s)`);
- console.log("Bot ("+client.user.tag+") Ready!");
- });
- 
-client.on("message", async message => {
+function randomString(length) {
+  var result = "";
+  chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for (var i = length; i > 0; --i)
+    result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+}
+
+client.on("message", async (message) => {
   const prefix = "-";
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  if(command === "nitro"){
-	var i = 0;
-    while (i<6000) { //Number of messages
-    var password = generator.generate({
-    length: 16,
-    numbers: true
-});
-	message.channel.send("https://discord.gift/" + password)
-    i++;
-	}
+  if (command === "nitro") {
+    codes = [];
+    for (let i = 0; i <= 6000; i++) {
+      codes.push("discord.gift/" + randomString(16));
+      if (i >= 6000) {
+        message.channel.send(
+          message.author.toString() + " Check Your DM's for the codes"
+        );
+        fname = randomString(5) + "codes.txt";
+        fs.writeFileSync(__dirname + "/" + fname);
+        message.author.send({ files: [__dirname + "/" + fname] }).then(() => {
+          fs.unlinkSync(__dirname + "/" + fname);
+          console.log("Sent 6,000 codes to " + message.author.tag);
+        });
+      }
+    }
   }
-})
-
+});
 
 client.login(process.env.token);
